86c779e5436af7f9a97f71b8489e0fb567f8734e
Shailesh Vashishth <shavashishth@gmail.com>
2025-07-20 12:19:30 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(unused_imports)]
 use std::net::TcpListener;
 use std::io::Write;
+use std::io::Read;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -13,6 +14,14 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
+                let mut buf = [0;512];
+                loop {
+                    let read_count = _stream.read(&mut buf).unwrap();
+                    if read_count == 0 {
+                        break;
+                    }
+                    _stream.write_all(b"+PONG\r\n").unwrap();
+                }
                 println!("accepted new connection");
                 _stream.write_all(b"+PONG\r\n").unwrap();
             }
